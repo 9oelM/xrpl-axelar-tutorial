25c55906fa2ff9843106cb0db620daec978f361e
9oelM <hj923@hotmail.com>
2025-02-25 16:48:28 +0900

    wip

--- script/Bank.s.sol
+++ script/Bank.s.sol
@@ -7,12 +7,17 @@ import {Bank} from "../src/Bank.sol";
 contract BankScript is Script {
     Bank public bank;
 
+    address ITS = address(0x1a7580C2ef5D485E069B7cf1DF9f6478603024d3);
+
     function setUp() public {}
 
     function run() public {
+        uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
         vm.startBroadcast();
 
-        bank = new Bank(address(0x11));
+        bank = new Bank(ITS);
 
         vm.stopBroadcast();
     }
